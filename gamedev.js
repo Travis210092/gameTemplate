c4b1c9e3af28afa64d5a185da7957088d4d59fdf
Btang516 <210229@hkis.edu.hk>
2015-12-15 10:15:48 +0800

    Changed buttons cause my boss isn't happy with me plz up my pay

--- gamedev.js
+++ gamedev.js
@@ -1,11 +1,19 @@
 $( document ).ready(function() {
 
-	$("#goRight1").click(function() {
+	$("#goRightB1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreRight1.jpg')");
 		$("#goRight1").hide();
 		$("#goBack").hide();
 		$("#goLeftR2B").show();
 		$("#goRightR2B").show();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 
 	$("#goRightL2B").click(function() {
@@ -29,51 +37,138 @@ $( document ).ready(function() {
 		$("#exploreBody").css("background-image", "url('img/explore2-goBack2.jpg')");
 		$("#goExitL4").show();
 		$("#goExitR4").show();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 
 
-	$("#goLeft1").click(function() {
+	$("#goLeftB1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft1.jpg')");
 		$("#goLeftL2B").show();
 		$("#goRightL2B").show();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 
 	$("#goLeftL2B").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft2.jpg')");
 		$("#goLeft2").hide();
 		$("#goExit1").show();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit3").hide
 	});
 
 	$("#goLeftR2B").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").show();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 
 	$("#goExit3").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").hide();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 	$("#goExit1").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").hide();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 	$("#goExit2").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").hide();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 	$("#goExitR4").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").hide();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 	$("#goExitL4").click(function() {
 		$("#exploreBody").css("background-image", "url('img/exploreLeft3.jpg')");
 		$("#goLeft3").hide();
 		$("#goExit3").hide();
+		$("#goRight-Straight1").hide();
+		$("#goExit2").hide();
+		$("#goRightL2B").hide
+		$("#goRight1").hide
+		$("#goRightR2B").hide
+		$("#goLeftL2B").hide
+		$("#goLeft1").hide
+		$("#goLeftR2B").hide
+		$("#goExit1").hide
+		$("#goExit3").hide
 	});
 
 	
